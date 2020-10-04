d0c39f99a4b4e021e8e180e2010676e64bd4cd40
Dominik 'Dragenet' Domagalski <dragenetdev@gmail.com>
2020-10-04 22:53:35 +0200

    Main page with hero created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
+import { graphql } from 'gatsby';
+import Button from '../components/Button/Button';
+import Image from 'gatsby-image';
 
-const IndexPage = () => (
+const ContentWrapper = styled.div`
+  width: 60%;
+  height: calc(100vh - 80px);
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+
+  h1 {
+    font-size: 105px;
+    margin: 0;
+    width: 60%;
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 60px 0 40px;
+    width: 40%;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  position: absolute !important;
+  top: 0;
+  right: 0;
+  width: 40%;
+  height: 100vh;
+
+  img {
+    object-position: 0 !important;
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <>
-    <h1>Hello HATTA</h1>
+    <ContentWrapper>
+      <h1>Your new space</h1>
+      <p>
+        While artists work from real to the abstract, architects must work from
+        the abstract to the real.
+      </p>
+      <Button>estimate project</Button>
+    </ContentWrapper>
+    <StyledImage fluid={data.file.childImageSharp.fluid} />
   </>
 );
 
+export const query = graphql`
+  {
+    file(name: { eq: "hero" }) {
+      childImageSharp {
+        fluid(maxWidth: 800, maxHeight: 1200, quality: 100) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
+
 export default IndexPage;
+
+// duotone: { highlight: "#f00e2e", shadow: "#192550", opacity: 0 }
